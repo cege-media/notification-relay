e7a74d7f727377fbf8f0bede7935aae7d2eda560
Will Ayers <willayers.net@gmail.com>
2020-07-20 12:16:29 -0700

    feat: setup logger with winston 3.x and winston-cloudwatch

--- src/config/example-web/tcpclient.js
+++ src/config/example-web/tcpclient.js
@@ -4,26 +4,25 @@
  * This will create a TCP client and connect to the TCP server at the specified server address and port
  */
 
+var net = require("net");
 
-var net = require('net');
-
-var server = '127.0.0.1';
-var port = 9000;
+var server = "127.0.0.1";
+var port = 9999;
 
 var client = new net.Socket();
 
-client.connect(port, server, function() {
-	console.log('Connected');
+client.connect(port, server, function () {
+  console.log("Connected");
 
-	// Send cacheinit message
-	client.write('cacheinit');
+  // Send cacheinit message
+  client.write("cacheinit");
 });
 
-client.on('data', function(data) {
-	console.log('Received: ' + data);
-	//client.destroy();
+client.on("data", function (data) {
+  console.log("Received: " + data);
+  //client.destroy();
 });
 
-client.on('close', function() {
-	console.log('Connection closed');
+client.on("close", function () {
+  console.log("Connection closed");
 });
